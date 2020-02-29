0604d913522a8e282bb3d77050d551ff07f4b5e3
Karl Dusenbery <karl.dusenbery@gmail.com>
2020-02-29 15:40:09 +0800

    Finished the ViewHolder

--- app/src/main/java/com/dusenbery/listmaker/TodoListAdapter.kt
+++ app/src/main/java/com/dusenbery/listmaker/TodoListAdapter.kt
@@ -1,5 +1,6 @@
 package com.dusenbery.listmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
@@ -8,7 +9,9 @@ class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
     private val todoLists = arrayOf("Android Development", "House Work", "Errands")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.todo_list_view_holder, parent, false)
+        return TodoListViewHolder(view)
     }
 
     override fun getItemCount(): Int {
