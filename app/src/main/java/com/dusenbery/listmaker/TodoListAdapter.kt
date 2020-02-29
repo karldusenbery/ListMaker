43265527d4a5a6b1a92691211db0a8b959e19377
Karl Dusenbery <karl.dusenbery@gmail.com>
2020-02-29 15:20:15 +0800

    Added an array of dummy data to the adapter

--- app/src/main/java/com/dusenbery/listmaker/TodoListAdapter.kt
+++ app/src/main/java/com/dusenbery/listmaker/TodoListAdapter.kt
@@ -4,12 +4,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
+
+    private val todoLists = arrayOf("Android Development", "House Work", "Errands")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return todoLists.size
     }
 
     override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
