fc36535c17d04ac75d14e9a9ae8dcf78404acdbc
karldusenbery <1tNjzUKrtG6YlWW>
2020-03-16 14:32:49 +0800

    Converted to Single Activity Architecture by adjusting the layouts and moving MainActivity logic to the TodoListFragment.

--- app/src/main/java/dev/dusenbery/listmaker/TaskDetailFragment.kt
+++ app/src/main/java/dev/dusenbery/listmaker/TaskDetailFragment.kt
@@ -2,14 +2,22 @@ package dev.dusenbery.listmaker
 
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TaskDetailFragment : Fragment() {
 
     lateinit var list: TaskList
+    lateinit var taskListRecyclerView: RecyclerView
+    lateinit var addTaskButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +34,39 @@ class TaskDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_task_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity?.let {
+            taskListRecyclerView = view.findViewById(R.id.task_list_recyclerview)
+            taskListRecyclerView.layoutManager = LinearLayoutManager(it)
+            taskListRecyclerView.adapter = TaskListAdapter(list)
+
+            addTaskButton = view.findViewById(R.id.add_task_button)
+            addTaskButton.setOnClickListener(){
+                showCreateTaskDialog()
+            }
+        }
+    }
+
+    private fun showCreateTaskDialog() {
+
+        activity?.let {
+            val taskEditText = EditText(it)
+            taskEditText.inputType = InputType.TYPE_CLASS_TEXT
+            AlertDialog.Builder(it)
+                .setTitle(R.string.task_to_add)
+                .setView(taskEditText)
+                .setPositiveButton(R.string.add_task) {
+                        dialog, _ ->
+                    val task = taskEditText.text.toString()
+                    list.tasks.add(task)
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        }
+    }
 
     companion object {
 
