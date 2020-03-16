868ba65b6fefb704da360f84fc03bd4de8c510ee
karldusenbery <1tNjzUKrtG6YlWW>
2020-03-16 11:47:44 +0800

    Converted to Single Activity Architecture by adjusting the layouts and moving MainActivity logic to the TodoListFragment.

--- app/src/main/java/dev/dusenbery/listmaker/TodoListFragment.kt
+++ app/src/main/java/dev/dusenbery/listmaker/TodoListFragment.kt
@@ -1,18 +1,19 @@
 package dev.dusenbery.listmaker
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_todo_list.*
 
 class TodoListFragment : Fragment(), TodoListAdapter.TodoListClickListener {
 
-    private var listener: OnFragmentInteractionListener? = null
     private lateinit var todoListRecyclerView: RecyclerView
     private lateinit var listDataManager: ListDataManager
 
@@ -31,24 +32,18 @@ class TodoListFragment : Fragment(), TodoListAdapter.TodoListClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        activity?.let {
+            listDataManager = ListDataManager(it)
+        }
+
         val lists = listDataManager.readLists()
         todoListRecyclerView = view.findViewById(R.id.lists_recyclerview)
         todoListRecyclerView.layoutManager = LinearLayoutManager(activity)
         todoListRecyclerView.adapter = TodoListAdapter(lists, this)
 
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-            listDataManager = ListDataManager(context)
+        fab.setOnClickListener { _ ->
+            showCreateTodoListDialog()
         }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
     }
 
     interface OnFragmentInteractionListener {
@@ -63,7 +58,7 @@ class TodoListFragment : Fragment(), TodoListAdapter.TodoListClickListener {
     }
 
     override fun listItemClicked(list: TaskList) {
-        listener?.onTodoListClicked(list)
+
     }
 
     fun addList(list: TaskList) {
@@ -75,6 +70,32 @@ class TodoListFragment : Fragment(), TodoListAdapter.TodoListClickListener {
     fun saveList(list: TaskList) {
         listDataManager.saveList(list)
         updateLists()
+    }
+
+    private fun showCreateTodoListDialog() {
+        activity?.let {
+            val dialogTitle = getString(R.string.name_of_list)
+            val positiveButtonTitle = getString(R.string.create_list)
+            val myDialog = AlertDialog.Builder(it)
+            val todoTitleEditText = EditText(it)
+            todoTitleEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_WORDS
+
+            myDialog.setTitle(dialogTitle)
+            myDialog.setView(todoTitleEditText)
+
+            myDialog.setPositiveButton(positiveButtonTitle) {
+                    dialog, _ ->
+                val list = TaskList(todoTitleEditText.text.toString())
+                addList(list)
+                dialog.dismiss()
+                showTaskListItems(list)
+            }
+            myDialog.create().show()
+        }
+    }
+
+    private fun showTaskListItems(list: TaskList) {
+
     }
 
     private fun updateLists() {
